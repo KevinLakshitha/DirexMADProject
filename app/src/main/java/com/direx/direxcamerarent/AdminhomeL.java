c57e6a8a5874c60031e28563d8831f392559c73a
KevinLakshitha <87199453+KevinLakshitha@users.noreply.github.com>
2021-09-29 00:42:53 +0530

    new update

---
+++ app/src/main/java/com/direx/direxcamerarent/AdminhomeL.java
@@ -0,0 +1,14 @@
+package com.direx.direxcamerarent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminhomeL extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adminhome_l);
+    }
+}
