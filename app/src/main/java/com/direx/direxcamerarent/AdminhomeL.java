8d9f02046d6cc5ebdec0145256ecf9062879bf62
KevinLakshitha <87199453+KevinLakshitha@users.noreply.github.com>
2021-09-29 01:55:54 +0530

    next button

--- app/src/main/java/com/direx/direxcamerarent/AdminhomeL.java
+++ app/src/main/java/com/direx/direxcamerarent/AdminhomeL.java
@@ -2,7 +2,10 @@ package com.direx.direxcamerarent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AdminhomeL extends AppCompatActivity {
 
@@ -10,5 +13,40 @@ public class AdminhomeL extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adminhome_l);
+
+        Button Lenses = findViewById(R.id.btnLensAH);
+
+        Button Cushome = findViewById(R.id.btnCushome);
+
+        Lenses.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                opennext();
+
+
+            }
+        });
+
+        Cushome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                opennext1();
+
+
+            }
+        });
+    }
+
+
+    public void opennext() {
+        Intent intent1 = new Intent(this, LensAdmin.class);
+        startActivity(intent1);
+    }
+
+    public void opennext1() {
+        Intent intent2 = new Intent(this, CustomerLensList.class);
+        startActivity(intent2);
     }
 }
